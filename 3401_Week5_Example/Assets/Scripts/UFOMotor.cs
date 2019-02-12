ef0f4a0b5a07a988f9bd3d5f138871e64333f1ad
Michael Stephens <ms221409@ohio.edu>
2019-02-12 17:55:08 -0500

    Project commit

---
+++ 3401_Week5_Example/Assets/Scripts/UFOMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMotor : MonoBehaviour
+{
+    Vector3 targetPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton (0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast (cameraRay, out hit))
+            {
+                targetPosition = new Vector3(hit.point.x, hit.point.y + 5, hit.point.z);
+                //transform.position = targetPosition;
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
+    }
+}
